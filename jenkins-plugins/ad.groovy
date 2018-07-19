f3e2a1f64cb466e1e10c27bcfbc2300d9db33e00
Konstantinos Psimoulis <kosta@noima.com>
2018-07-19 08:48:00 -0400

    Adding AD and user configuration

---
+++ jenkins-plugins/ad.groovy
@@ -0,0 +1,12 @@
+import jenkins.model.*
+import hudson.security.*
+import hudson.plugins.active_directory.*
+
+def instance = Jenkins.getInstance()
+String domain = 'my.domain.com'
+String site = 'site'
+String server = '192.168.1.1'
+String bindName = 'account@my.domain.com'
+String bindPassword = 'password'
+adrealm = new ActiveDirectorySecurityRealm(domain, site, bindName, bindPassword, server)
+instance.setSecurityRealm(adrealm)
