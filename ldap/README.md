01a8fb9150f91840dc83e6521690030845ff16df
Athanasios Babouras <bbrthanasis@gmail.com>
2019-11-23 18:13:01 -0500

    Combines LDAP and WebDAV support for apache server with the phpmyadmin UI

---
+++ ldap/README.md
@@ -0,0 +1,9 @@
+# WebDAV and LDAP with phpmyadmin
+This solution creates 3 containers to run LDAP with WebDAV using phpmyadmin as a web UI.
+
+## Usage
+To run the containers simply use the docker command:
+
+```bash
+docker-compose up
+```
---
+++ ldap/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Add our dav location to the httpd config
+cat <<EOF > /usr/local/apache2/conf/extra/vife.conf
+LoadModule	dav_module           modules/mod_dav.so
+LoadModule  dav_fs_module        modules/mod_dav_fs.so
+LoadModule	ldap_module          modules/mod_ldap.so
+LoadModule	authnz_ldap_module   modules/mod_authnz_ldap.so
+DocumentRoot "/var/webdav"
+DavLockDB "/run/lock/apache/DavLock.db"
+
+PassEnv AuthLDAPURL
+PassEnv AuthLDAPBindDN
+PassEnv AuthLDAPBindPassword
+PassEnv RequireLDAPGroup
+
+# rewriting Destination because we're behind an SSL terminating reverse proxy
+# see http://www.dscentral.in/2013/04/04/502-bad-gateway-svn-copy-reverse-proxy/  
+RequestHeader edit Destination ^https: http: early
+
+<Directory "/var/webdav">
+    DAV on
+    Options +Indexes
+    IndexOptions Charset=UTF-8 FancyIndexing FoldersFirst
+    IndexIgnore .??* :2*
+    AllowOverride None
+    AuthName "Noima WebDAV: Login with username and password" 
+    AuthBasicProvider ldap
+    AuthType Basic
+    AuthLDAPGroupAttribute ${AuthLDAPGroupAttribute} 
+    AuthLDAPGroupAttributeIsDN ${AuthLDAPGroupAttributeIsDN}
+    AuthLDAPURL ${AuthLDAPURL}
+    AuthLDAPBindDN "${AuthLDAPBindDN}" 
+    AuthLDAPBindPassword "${AuthLDAPBindPassword}"
+    Require ldap-group ${RequireLDAPGroup}
+</Directory>
+EOF
